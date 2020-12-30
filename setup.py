65cd10a17ad525392b859996eb6e3611e3f4d9b2
Alejandro Caravaca Puchades <59510094+acpuchades@users.noreply.github.com>
2020-12-30 01:07:22 +0100

    Increase package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name="labtool",
-	version="0.0.29",
+	version="0.0.30",
 	author="Alejandro Caravaca Puchades",
 	author_email="acaravacapuchades+dev@gmail.com",
 	description="A tool to analyze lab report files",
